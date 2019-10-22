277ca0b0949431aeccf1c09fdfe62246ae0bbecc
rylynn-abo <rylynn_ab@163.com>
2019-10-22 20:59:14 +0800

    Initial commit

---
+++ src/models/transE.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: rylynn 
+@file: transE.py
+@time: 2019-10-22 20:49
+@discription: 
+"""
+from torch import nn
+
+
+class TransE(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(input):
+        pass
+
